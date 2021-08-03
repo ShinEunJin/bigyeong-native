de5e9e2e5a64832b3d1f53a828161860058832d0
ShinEunJin <sineun5501@naver.com>
2021-08-03 10:50:23 +0900

    generate key error handling and firestore user create logic

--- src/utils/generateKey.js
+++ src/utils/generateKey.js
@@ -1,14 +1,30 @@
 import * as Random from "expo-random"
 import * as SecureStore from "expo-secure-store"
+import moment from "moment"
+
+import { firestore } from "../../firebase"
 
 const generateSecureKey = async () => {
   const key = await SecureStore.getItemAsync("key")
   if (!key) {
-    let user = await SecureStore.setItemAsync(
-      "key",
-      Random.getRandomBytes(10).join("")
-    )
-    // firebase에 유저 바로 key값으로 유저 생성
+    try {
+      let random = Random.getRandomBytes(10).join("")
+      await SecureStore.setItemAsync("key", random)
+      // firebase에 유저 바로 key값으로 유저 생성
+      await firestore.collection("users").doc(random).set({
+        uploads: [],
+        likes: [],
+        created: moment().format(),
+      })
+    } catch (error) {
+      // key값은 생성됬지만 렉으로 인해 firestore유저가 생성 되지 못할
+      // 수 있으므로 도중에 에러가 생기면 key값 제거
+      console.log(error)
+      await SecureStore.deleteItemAsync("key")
+      alert(
+        "key값을 생성하는데 실패하였습니다 앱을 다시 시작하여 주시기 바랍니다"
+      )
+    }
   }
 }
 
