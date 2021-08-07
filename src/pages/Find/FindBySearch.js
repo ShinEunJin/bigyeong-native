26d4f32e37bcb7f80eaee1935f4ee4f6f788f370
ShinEunJin <sineun5501@naver.com>
2021-08-07 16:02:33 +0900

    search component make

--- src/pages/Find/FindBySearch.js
+++ src/pages/Find/FindBySearch.js
@@ -1,20 +1,95 @@
 import React, { useState } from "react"
-import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native"
+import {
+  ActivityIndicator,
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  Image,
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { useNavigation } from "@react-navigation/native"
+
 import { themeColor } from "../../theme"
+import searchLogic from "./searchLogic"
 
 const FindBySearch = () => {
+  const navigation = useNavigation()
+
   const [searchTerm, setSearchTerm] = useState("")
+  const [data, setData] = useState([])
+  // 데이터가 없을 때 noData는 true (검색 후 데이터가 없다고 표시할 때 쓰임)
+  const [noData, setNoData] = useState(false)
+  const [message, setMessage] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const onSearch = async (text) => {
+    setLoading(true)
+    const result = await searchLogic(text.toLowerCase())
+    if (!result) {
+      setNoData(true)
+      setMessage(`${searchTerm}에 대한 검색결과가 없습니다`)
+    } else {
+      setNoData(false)
+      setData(result)
+      setLoading(false)
+    }
+  }
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableHighlight
+        style={{ alignItems: "center" }}
+        onPress={() => navigation.navigate("Detail", { item })}
+      >
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{ uri: item.uri }} />
+        </View>
+      </TouchableHighlight>
+    )
+  }
 
   return (
     <View style={styles.mainContainer}>
-      <Text style={{ color: "white" }}>FindBySearch</Text>
-      <SafeAreaView>
+      {/* 검색 부분 */}
+      <Text style={{ color: "white", fontWeight: "bold", paddingVertical: 10 }}>
+        Search
+      </Text>
+      <SafeAreaView style={{ flexDirection: "row", marginBottom: 20 }}>
         <TextInput
           style={styles.inputStyle}
           value={searchTerm}
           onChangeText={setSearchTerm}
         />
+        <Pressable
+          onPress={() => onSearch(searchTerm)}
+          style={styles.searchBtn}
+        >
+          <Icon name="search" color="black" size={25} />
+        </Pressable>
       </SafeAreaView>
+      {/* 결과 부분 */}
+      {noData ? (
+        <View style={styles.messageContainer}>
+          <Text style={{ color: "white" }}>{message}</Text>
+        </View>
+      ) : (
+        <View style={{ flex: 1, width: "100%" }}>
+          {loading ? (
+            <ActivityIndicator color="white" size="large" />
+          ) : (
+            <FlatList
+              data={data}
+              keyExtractor={(_, index) => index.toString()}
+              renderItem={renderItem}
+            />
+          )}
+        </View>
+      )}
     </View>
   )
 }
@@ -23,7 +98,6 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
     backgroundColor: themeColor.defaultBackgroundColor,
   },
   inputStyle: {
@@ -33,6 +107,32 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: themeColor.defaultFontColor,
     color: themeColor.defaultFontColor,
+    marginRight: 10,
+  },
+  searchBtn: {
+    borderWidth: 1,
+    backgroundColor: themeColor.defaultFontColor,
+    borderRadius: 10,
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  messageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: "90%",
+    marginBottom: 30,
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: 220,
+    borderRadius: 10,
+    backgroundColor: "gray",
   },
 })
 
--- src/pages/Find/searchLogic.js
+++ src/pages/Find/searchLogic.js
@@ -1,7 +1,30 @@
-const search = async (text) => {
-  const term = "경남 여좌천에 오세요"
-  let result = text.trim()
-  new RegExp()
+import { firestore } from "../../../firebase"
+
+const searchLogic = async (text) => {
+  // 특수문자 및 띄어쓰기 제거
+  let special = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\" ]/gi
+  let cleanText = text.replace(special, "")
+  // 한글 체크
+  let checkKor
+  let korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g
+  checkKor = korean.test(cleanText)
+  let list = []
+  if (checkKor) {
+    const placeRef = firestore.collection("places")
+    const result = await placeRef.where("title", ">=", cleanText).get()
+    result.forEach((item) => {
+      list.push(item.data())
+    })
+  } else {
+    const placeRef = firestore.collection("places")
+    const result = await placeRef.where("title", "<=", cleanText).get()
+    result.forEach((item) => {
+      list.push(item.data())
+    })
+  }
+  if (list.length > 0) return list
+  else return null
+  // 이렇게 해도 일단 데이터는 뒤죽박죽으로 나온다. 나중에 검색 엔진 서비스를 도입해야한다
 }
 
-export default search
+export default searchLogic
