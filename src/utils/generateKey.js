128c35e5b49649a29aaffe0411e426a01843e866
ShinEunJin <sineun5501@naver.com>
2021-08-09 14:44:43 +0900

    logo fixed

--- src/utils/generateKey.js
+++ src/utils/generateKey.js
@@ -7,7 +7,6 @@ import { firestore } from "../../firebase"
 const generateSecureKey = async () => {
   // 키 값 가져오기
   const key = await SecureStore.getItemAsync("key")
-  // 키 값은 있는데 혹여나 유저 데이터가 삭제 되었을 경우 다시 생성
 
   if (!key) {
     try {
@@ -30,6 +29,7 @@ const generateSecureKey = async () => {
     }
   }
 
+  // 키 값은 있는데 혹여나 유저 데이터가 삭제 되었을 경우 다시 생성
   const userRef = firestore.collection("users").doc(key)
   const user = await userRef.get()
   if (!user.exists) {
